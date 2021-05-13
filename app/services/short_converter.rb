ad95280aa5738a7188cb993f1f35009046e4f9d0
Gergely Karolyi <gergelykarolyi@gmail.com>
2021-05-13 19:57:08 +0200

    add x86_64-linux platform for heroku

--- app/services/short_converter.rb
+++ app/services/short_converter.rb
@@ -1,4 +1,7 @@
 module ShortConverter
+  ##
+  # Base service class which provides an alphabet 
+  # and a class method for checking its length.
   class ConverterBase
     ALPHABET = (('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a).freeze
 
@@ -7,7 +10,11 @@ module ShortConverter
     end
   end
 
+  ##
+  # Service class for encoding numbers into strings.
   class Encoder < ConverterBase
+    # Bijective function which takes an id
+    # (Integer) and returns its base64 equivalent (String)
     def self.call(id)
       return ALPHABET.first if id.zero?
 
@@ -21,7 +28,12 @@ module ShortConverter
     end
   end
 
+  ##
+  # Service class for decoding strings into numbers.
   class Decoder < ConverterBase
+    # Bijective function which takes
+    # a base64 shortcode (String) and returns its base10
+    # equivalent (Integer).
     def self.call(short)
       i = 0
       short.each_char { |char| i = i * base + ALPHABET.index(char) }
